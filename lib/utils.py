59e619ffb97dbd2b7adee55c9717b34090cec04f
Pulkit <pgoel3379@gmail.com>
2023-12-17 20:55:42 +0530

    remove unnecessary comments

--- lib/utils.py
+++ lib/utils.py
@@ -15,10 +15,7 @@ def transform_image(image):
         transforms.CenterCrop([28, 28]),
         transforms.ToTensor()
     ])
-
-    # the tensor right now contains 4 channels for rgba, see which ones are useful to us 
-    # the final tensor going for predictions should have a single monochromatic channel. 
-    # the last channel has most of the data we need, find what to do with the rest 3. 
+    
     data = image.split(b',')[-1]
     image_data = base64.decodebytes(data) 
     pil_image = Image.open(BytesIO(image_data))
